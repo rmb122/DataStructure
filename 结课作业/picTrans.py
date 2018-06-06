6e4152effe56cf13ae77783a7c9a55b0c2826f2a
rmb122 <rmb122>
2018-06-06 12:06:38 +0800

    update

--- 结课作业/picTrans.py
+++ 结课作业/picTrans.py
@@ -7,7 +7,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.setFixedSize(458, 96)
+        MainWindow.setFixedSize(430, 96)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
