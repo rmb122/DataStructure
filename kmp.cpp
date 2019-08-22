a4ab6edb69297a51de03059860a3252fd0f34812
rmb122 <me@rmb122.cn>
2019-08-22 13:43:39 +0800

    Fix bug

--- kmp.cpp
+++ kmp.cpp
@@ -11,17 +11,20 @@ using std::endl;
 
 
 vector<unsigned int> getNextPos(string str) {
-    vector<unsigned int> nextPos(str.size(), UINT32_MAX);
-    unsigned int currPos = 1;
-    unsigned int lastNextPos = UINT32_MAX;
+    vector<unsigned int> nextPos = {UINT32_MAX};
+    nextPos.resize(str.size(), 0);
+
+    unsigned int currPos = 2;
+    unsigned int lastNextPos = 0;
 
     while(currPos < str.size()) {
-        if(lastNextPos == UINT32_MAX || str[currPos - 1] == str[lastNextPos]) {
+        if(str[currPos - 1] == str[lastNextPos]) {
+            nextPos[currPos] = lastNextPos + 1;
             lastNextPos++;
-            nextPos[currPos] = lastNextPos;
             currPos++;
         } else {
-            lastNextPos = nextPos[lastNextPos];
+            lastNextPos = 0;
+            currPos++;
         }
     }
     return nextPos;
@@ -35,16 +38,20 @@ unsigned int kmp(string str, string subStr, unsigned int start = 0) {
     vector<unsigned int> nextPos = getNextPos(subStr);
     unsigned int strPos = start;
     unsigned int subStrPos = 0;
-    while(strPos < str.size()) {
-        if(subStrPos == UINT32_MAX || str[strPos] == subStr[subStrPos]) { //如果是匹配失败(子串位置归零, 从头开始匹配)或者当前子串正确(查找下一个字符是否正确)
+    while(strPos < str.size() && (subStrPos < subStr.size())) {
+        if(str[strPos] == subStr[subStrPos]) { 
             subStrPos++;
             strPos++;
         } else {
             subStrPos = nextPos[subStrPos]; //回到上一个匹配的地方
-        }
+            if (subStrPos == UINT32_MAX) {
+                subStrPos = 0;
+                strPos++;
+            }  
+         }
     }
     if(subStrPos == subStr.size()) {
-        return strPos - subStrPos + 1; //配到子串, 返回匹配到的 Index
+        return strPos - subStrPos; //配到子串, 返回匹配到的 Index
     } else {
         return UINT32_MAX; //未能匹配到子串, 返回 UINT32_MAX
     }
@@ -52,6 +59,7 @@ unsigned int kmp(string str, string subStr, unsigned int start = 0) {
 
 
 int main() {
-    auto a = kmp("abcdabfabcdabc", "abcdabc");
+    auto a = kmp("caksjdoquwejkhdkkjaksabcdeabcdeabcdf123psaodo", "abcdeabcdf");
+    printf("%d", a);
     return 0;
 }
